558623a81122a27f675f21bbce0a9e7ab1ae854d
Sachin Warude <sachinwarude2794@gmail.com>
2026-08-25 20:53:37 +0530

    Changes in routes and its discriptions

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -9,10 +9,22 @@ const authMiddleware = require("../middleware/auth.middleware")
  */
 authRouter.post("/register",authcontroller.RegisterUser)
 
+/**
+ * @route POST /api/auth/login
+ * @description : Login the existing user 
+ */
 authRouter.post("/login",authcontroller.LoginUser)
 
+/**
+ * @route POST /api/auth/logout
+ * @description : Logout the current user 
+ */
 authRouter.post("/logout", authcontroller.UserLogout)
 
+/**
+ * @route GET /api/auth/get-me
+ * @description : get the user data
+ */
 authRouter.get("/get-me", authMiddleware.AuthUser,authcontroller.getme)
 
 
